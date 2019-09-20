eab43ee2ed8f5171f7d152bfbddf1369458dec67
Sophia <gmb728@gmail.com>
2019-09-20 08:47:38 +0800

    Initial Commit

---
+++ SwiftUIPath/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIPath
+//
+//  Created by Chang Sophia on 9/20/19.
+//  Copyright © 2019 Chang Sophia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
